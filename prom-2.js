eb2db69d3b77b6030ad246bbbb36a780f5dedfec
Cristian Sanchez Flores <andygign40@gmail.com>
2022-09-22 23:02:46 -0500

    Manejo de errores en las promesas

--- prom-2.js
+++ prom-2.js
@@ -2,6 +2,13 @@
 function sumarUno(numero){
 
     var promesa = new Promise(function(resolve, reject){
+
+        console.log(numero);
+
+        if (numero >= 7) {
+            reject('El numero es muy alto');
+        }
+
         setTimeout(function(){
             //return numero + 1;
             //callback(null, numero + 1);
@@ -12,21 +19,50 @@ function sumarUno(numero){
     return promesa;
 }
 
-//sumarUno(5).then(function(nuevoNumero){
-// sumarUno(5).then( nuevoNumero => {
-//     sumarUno(nuevoNumero).then(nuevoNumero2 =>{
-//         console.log(nuevoNumero2);
-//     });
-// });
+/**sumarUno(5).then(function(nuevoNumero){
+ sumarUno(5).then( nuevoNumero => {
+     sumarUno(nuevoNumero).then(nuevoNumero2 =>{
+         console.log(nuevoNumero2);
+     });
+});*/
 
-sumarUno(5).then( nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno(nuevoNumero);
-})
-.then(nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno(nuevoNumero);
-})
-.then(nuevoNumero => {
-    console.log(nuevoNumero);
-});
+/**sumarUno(5)
+    .then(nuevoValor => {
+        console.log(nuevoValor);
+    })
+    .then( nuevoNumero => {
+        //console.log(nuevoNumero);
+        return sumarUno(nuevoNumero);
+    })
+    //.then(nuevoValor => sumarUno(nuevoValor))
+    .then( sumarUno)
+    .then(nuevoNumero => {
+        console.log(nuevoNumero);
+        return sumarUno(nuevoNumero);
+    })
+    .then(sumarUno)
+    .then(nuevoNumero => {
+        console.log(nuevoNumero);
+    });
+    .catch(error => {
+        console.log('Error en la promesa');
+        console.log(error);
+    });*/
+
+    sumarUno(0)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(sumarUno)
+        .then(nuevoNumero => {
+            console.log(nuevoNumero);
+        })
+        .catch(error => {
+            console.log('Error en la promesa');
+            console.log(error);
+        })
