272eec3c3eab8073a9ecddea7bb470f2a4b5544b
Cristian Sanchez Flores <andygign40@gmail.com>
2022-09-23 00:10:19 -0500

    Fetch Blob

---
+++ fetch-4.js
@@ -0,0 +1,15 @@
+
+let img = document.querySelector('img');
+
+fetch( 'superman.png' )
+    /** .then(resp => {
+        return resp.blob()
+    });*/
+    .then( resp => resp.blob() )
+    .then( imagen => {
+
+        // console.log(imagen);
+        var imgPath = URL.createObjectURL( imagen );
+        img.src = imgPath;
+
+    });
